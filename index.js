915b9cc7912691e9f1f92593dfe1e9db7870dce8
Alexandre Salvador <62520840+alexandresalvador@users.noreply.github.com>
2022-10-19 15:34:29 -0300

    atualizando

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
+// app.set('view engine', 'ejs');
+// app.use(express.static('public'));
+app.use(express.json())
 
-app.get('/teste', () =>{
-    console.log('Estamos no caminho certo !!')
-})
 
-app.listen(3000)
+//Rota que retorna na Home.
+app.get('/', function (req, res) {
+  res.send('Esta é a Home');
+});
+
+
+
+//INICIALIZANDO O APP!
+app.listen(3000, (erro) => {
+
+      if (erro) {
+          console.log(erro, 'Erro');
+
+      } else {
+          console.log(' Funcionando aqui: http://localhost:3000');
+     }
+});
+
+
+
+
+
+
+
+
---
+++ models/pessoa.js
@@ -0,0 +1,55 @@
+const DB = {
+    pessoa: [
+        {
+            id: 1,
+            nome: "Arthur",
+            email: "arthur@123",
+            genero: "Masculino",
+            idade: "26",
+            endereço: [
+                {
+                 rua: "Antonio Jorge Cecyn",
+                 número: 379,
+                 bairro: "Saguaçu",
+                 cidade: "Joinville",
+                 estado: "SC",
+                 cep: "30220749",
+                }
+            ],
+        },
+        {
+            id: 2,
+            nome: "Mário",
+            email: "mario@123",
+            genero: "Masculino",
+            idade: "25",
+            endereço: [
+                {
+                 rua: "pica-pau",
+                 número: 274,
+                 bairro: "Aventureiro",
+                 cidade: "Joinville",
+                 estado: "SC",
+                 cep: "10226549",
+                }
+            ],
+        },
+        {
+            id: 3,
+            nome: "Vírginia",
+            email: "virginia@123",
+            genero: "Feminino",
+            idade: "20",
+            endereço: [
+                {
+                 rua: "Flores",
+                 número: 100,
+                 bairro: "Bucarein",
+                 cidade: "Joinville",
+                 estado: "SC",
+                 cep: "32770741",
+                }
+            ],
+        },
+    ],
+};
